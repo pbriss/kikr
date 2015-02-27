c0195630b96fce429dcd95b9bf0ff23383d8ed0d
Pascal Brisset <pbriss@gmail.com>
2015-02-27 15:40:07 -0800

    Custom page directive

--- client/scripts/shared/app.directives.js
+++ client/scripts/shared/app.directives.js
@@ -4,20 +4,48 @@
 angular.module("app.directives", [])
 
 
-.directive('toggleSidebar', [function() {
-    return {
-        restrict: 'A',
-        link: function(scope, el, attrs) {
+	.directive('toggleSidebar', [function() {
+		return {
+			restrict: 'A',
+			link: function(scope, el, attrs) {
 
-            el.click(function(e) {
-                //Make sure the target is not the floating sidebar itself
-                if ($(e.target).is('i') || $(e.target).is('a')) {
-                    scope.sidebarOpen = scope.sidebarOpen ? false : true;
-                }
-            });
-        }
-    }
-}])
+				el.click(function(e) {
+					//Make sure the target is not the floating sidebar itself
+					if ($(e.target).is('i') || $(e.target).is('a')) {
+						scope.sidebarOpen = scope.sidebarOpen ? false : true;
+					}
+				});
+			}
+		}
+	}])
+
+
+		// add full body class for custom pages.
+	.directive('customPage', [function() {
+		return {
+			restrict: 'A',
+			controller: ['$scope', '$element', '$location', function($scope, $element, $location) {
+				$(window).load(function() {
+					var path = function() {return $location.path()};
+					var addBg = function(path) {
+						$element.removeClass('body-full');
+						switch(path) {
+							case '/404' :
+								$element.addClass('body-full');
+						}
+					};
+
+					addBg($location.path());
+
+					$scope.$watch(path, function(newVal, oldVal) {
+						if(angular.equals(newVal, oldVal)) return;
+						addBg($location.path());
+					})
+				});
+
+			}]
+		}
+	}])
 
 
 }());
