bc180b2088974e5cb1fba8c0f8e8934428558d37
Pascal Brisset <pbriss@gmail.com>
2015-02-27 16:34:48 -0800

    GoPro api npm

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,26 @@
 var PythonShell = require('python-shell');
+var fs = require('fs');
+var GoPro = require('goproh4');
+var cam = new GoPro.Camera();
 
 exports.btstart = function(req, res) {
-	var pyshell = new PythonShell('bluetooth.py', {mode: 'binary'});
+
+	fs.writeFile('python/state.txt', 1, function(err) {
+		if(err) {
+			console.log(err);
+		} else {
+			//Start bluetooth reading
+			new PythonShell('bluetooth.py', {mode: 'binary'});
+
+			//Start GoPro recording
+			cam.start().then(function () {
+				var msg = 'Bluetooth & GoPro started!';
+				console.log(msg);
+
+				res.json();
+			});
+		}
+	});
 };
 
 exports.btstop = function(req, res) {
