f39ebdef923976adae3b6596156e2dfdf667118e
Yann Ly-Gagnon <yann@yannmac-2.local>
2015-02-27 20:44:54 -0800

    Update metadata.py

--- python/metadata.py
+++ python/metadata.py
@@ -28,7 +28,7 @@ def main(infilename, outfilename):
         line, = plot([ 2, 3, 4 ])
 
 
-    pdb.set_trace()
+    #pdb.set_trace()
     fout = open(outfilename, 'w+')
 
 
@@ -125,22 +125,77 @@ def main(infilename, outfilename):
     tyd = np.arange(0,len(diff_y),1)
     tzd = np.arange(0,len(diff_z),1)
 
+    diffm_x = numpy.diff(magn_x_all_fl)
+    diffm_y = numpy.diff(magn_y_all_fl)
+    diffm_z = numpy.diff(magn_z_all_fl)
+    txdm = np.arange(0,len(diffm_x),1)
+    tydm = np.arange(0,len(diffm_y),1)
+    tzdm = np.arange(0,len(diffm_z),1)
+
+    diffg_x = numpy.diff(gyro_x_all_fl)
+    diffg_y = numpy.diff(gyro_y_all_fl)
+    diffg_z = numpy.diff(gyro_z_all_fl)
+    txdg = np.arange(0,len(diffg_x),1)
+    tydg = np.arange(0,len(diffg_y),1)
+    tzdg = np.arange(0,len(diffg_z),1)
+
+
+
     maxval = max([max(abs(diff_x)), max(abs(diff_y)), max(abs(diff_z))])
-    factor = 0.7
+    factor = 0.4
+
+    maxvalm = max([max(abs(diffm_x)), max(abs(diffm_y)), max(abs(diffm_z))])
+    factor = 0.4
+
+    maxvalg = max([max(abs(diffg_x)), max(abs(diffg_y)), max(abs(diffg_z))])
+    factor = 0.4
     
 
 
-    pdb.set_trace()
+    #pdb.set_trace()
     inds_x = numpy.nonzero(abs(diff_x)>maxval* factor)
     inds_y = numpy.nonzero(abs(diff_y)>maxval* factor)
     inds_z = numpy.nonzero(abs(diff_z)>maxval* factor)
     
     jumpdet = concatenate([inds_x[0], inds_y[0], inds_z[0]])
 
-    pdb.set_trace()
-    for x in numpy.nditer(jumpdet):
+
+    indsm_x = numpy.nonzero(abs(diffm_x)>maxval* factor)
+    indsm_y = numpy.nonzero(abs(diffm_y)>maxval* factor)
+    indsm_z = numpy.nonzero(abs(diffm_z)>maxval* factor)
+    jumpdetm = concatenate([indsm_x[0], indsm_y[0], indsm_z[0]])
+
+    indsg_x = numpy.nonzero(abs(diffg_x)>maxval* factor)
+    indsg_y = numpy.nonzero(abs(diffg_y)>maxval* factor)
+    indsg_z = numpy.nonzero(abs(diffg_z)>maxval* factor)
+    jumpdetg = concatenate([indsg_x[0], indsg_y[0], indsg_z[0]])
+
+
+    # Subsampling the Z : (subsample max)
+    # Remove the sample close to each other: 
+    indsg_z_clean = indsg_z[0]
+    for x in indsg_z_clean:
+        #Make sure they are unique: 
+        curv = numpy.nonzero( indsg_z_clean  ==  x )[0] 
+        #pdb.set_trace()
+    
+        if len(curv) > 0:
+            nonuniquearr = numpy.nonzero(abs(indsg_z_clean-x) < 5 ) 
+            for nonunique in nonuniquearr[0]:
+                if nonunique != curv[0]:
+                #curelement_index = numpy.nonzero( indsg_z_clean  ==  nonunique )[0] 
+                    indsg_z_clean = np.delete(indsg_z_clean,nonunique )
+                    print x
+
+
+
+    #pdb.set_trace()
+    for x in numpy.nditer(indsg_z_clean):
         print x
-        fout.write("T=" + str(x) )
+        # Get index when it happened: 
+        curidx = numpy.nonzero( diffg_z  ==  x )[0] 
+        pdb.set_trace()
+        fout.write("T=" + str(timearr[x]) +  " Jump" + "\n" )
 
     fout.close()
 
@@ -176,9 +231,17 @@ def main(infilename, outfilename):
 
         #now show detection: 
         figure(4)
-
         plot(txd, diff_x, tyd, diff_y, tzd, diff_z)
         draw() # update the plot
+
+        figure(5)
+        plot(txdm, diffm_x, tydm, diffm_y, tzdm, diffm_z)
+        draw() # update the plot
+
+        figure(6)
+        plot(txdg, diffg_x, tydg, diffg_y, tzdg, diffg_z)
+        draw() # update the plot
+
         
 
 # end        
@@ -213,7 +276,7 @@ if __name__ == "__main__":
         outfilename = 'python/metadata.txt'
 
 
-    pdb.set_trace()
+    #pdb.set_trace()
 
     if len(sys.argv) > 1:
         infilename = sys.argv[1]
