eb79c3e1e846a90ae64c96470665d6a6df262301
Yann Ly-Gagnon <yann@yannmac-2.local>
2015-02-28 11:39:24 -0800

    Add file to retrieve latest mp4

--- python/retrievelatest.py
+++ python/retrievelatest.py
@@ -3,10 +3,14 @@
 
 import os
 import glob
+import shutil
+import pdb
 
 
 newest = max(glob.iglob('/Volumes/NO NAME/DCIM/100GOPRO/*.[Mm][Pp]4'), key=os.path.getctime)
 
 
+pdb.set_trace()
+copyfile(newest, 'latest.mp4')
 
 print newest
