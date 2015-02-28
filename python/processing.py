27bc18506c647627ad6783c385aa9aefdd8f56c7
Yann Ly-Gagnon <yann@yannmac-2.local>
2015-02-28 12:30:25 -0800

    Update processing to get metadata

--- python/processing.py
+++ python/processing.py
@@ -1,14 +1,59 @@
 import os
+import pdb
+import re
+import sys
 
 from moviepy.editor import *
 
-# Grab original clip
-video = VideoFileClip('latest.mp4').subclip(2, 4)
 
-# Create thumbnail
-video.save_frame('videos/segments/thumb1.png')
+def main(isLocal):
+    #get the metadata.txt file : 
+    f = open('metadata.txt', 'r')
+    metaline = f.readline()
 
-result = CompositeVideoClip([video])
+    StartNegOffset = 3  # How many seconds before the jump
+    ClipLength = 10  # in seconds 
 
-# Save and crop segment
-result.write_videofile('videos/segments/vid1.mp4', audio=True)
+    
+    JumpLocation = int(re.findall('T=(\d+)', metaline)[0])
+    startclip = JumpLocation - StartNegOffset
+    endclip = startclip + ClipLength
+
+
+    if (isLocal):
+        makevideoclip( startclip , endclip , 'videos/segments/jump1')
+    else:
+        makevideoclip( startclip , endclip , 'python/videos/segments/jump1')        
+
+
+
+def makevideoclip(startclip, endclip, fileout):
+
+    # Grab original clip
+    video = VideoFileClip('latest.mp4').subclip(startclip, endclip)
+
+    # Create thumbnail
+    video.save_frame( fileout + '.png')
+    
+    result = CompositeVideoClip([video])
+    
+    # Save and crop segment
+    result.write_videofile( fileout + '.mp4', audio=True)
+
+
+
+if __name__ == "__main__":
+
+
+
+    val = ''
+    if (len(sys.argv) > 1):
+        val = sys.argv[1]
+
+    isLocal = False
+    if val == 'local':
+        isLocal =	True
+        
+    
+
+    main(isLocal)
