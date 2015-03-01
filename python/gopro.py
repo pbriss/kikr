63e8a2b98cc6d9cb9cbc9b7348b35f835db5e567
Yann Ly-Gagnon <yann@yannmac-2.local>
2015-02-28 21:19:07 -0800

    Make gopro deamon

--- python/gopro.py
+++ python/gopro.py
@@ -28,8 +28,6 @@ while True:
             alreadystart = True
             process = Popen("ffmpeg -y -i http://10.5.5.9:8080/live/amba.m3u8 -c:v copy -an python/videos/src/test.mp4", shell=True)
     else:
-
         if '0' in strr:
-                        
             os.kill(process.pid, signal.SIGTERM)
-            break
+            alreadystart = False
