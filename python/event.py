1004af9a8b8fcc0b3654c26ed5a733cb19e4c897
Yann Ly-Gagnon <yannlygagnon@gmail.com>
2015-03-04 12:43:00 -0800

    Update code

--- python/event.py
+++ python/event.py
@@ -2,9 +2,11 @@ class Event(object):
     move = ""
     start = 0
     end = 0
+    detindex = 0
 
     # The class "constructor" - It's actually an initializer
-    def __init__(self, start, end, move):
+    def __init__(self, start, end, move, detindex):
         self.move = move
         self.start = start
         self.end = end
+        self.detindex = detindex
